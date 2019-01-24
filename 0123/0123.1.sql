28b6afbd772d45c910d3c82aa77a1ce33a2e6761
s18014 <s18014@std.it-college.ac.jp>
2019-01-24 00:45:29 +0000

    0123の課題を追加

---
+++ 0123/0123.1.sql
@@ -0,0 +1,10 @@
+drop table my_employees
+/
+create table my_employees(
+    id number(4) not null,
+    last_name varchar2(25),
+    first_name varchar2(25),
+    userid varchar(8),
+    salary number(9,2)
+)
+/
---
+++ 0123/0123.2.sql
@@ -0,0 +1,3 @@
+insert into my_employees
+values (1, 'Patel', 'Ralph', 'rpatel', 895)
+/
